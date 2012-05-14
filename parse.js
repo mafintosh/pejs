135ede393bc3c9ce5c0126d2aa22e5bc10ac542f
Mathias Buus <mathiasbuus@gmail.com>
2012-05-14 23:10:49 +0200

    added new a much better parser

---
+++ parse.js
@@ -0,0 +1,58 @@
+var STATIC = 'STATIC';
+var LOGIC = 'LOGIC';
+var EXPRESSION = 'EXPRESSION';
+var ESCAPE_EXPRESSION = 'ESCAPE_EXPRESSION';
+var BLOCK_DECLARE = 'BLOCK_DECLARE';
+var BLOCK_OVERRIDE = 'BLOCK_OVERRIDE';
+var BLOCK_ANONYMOUS = 'BLOCK_ANONYMOUS';
+
+var MATCH_BLOCK = /^(\w+)?\s*((?:'(?:(?:\\')|[^'])*')|(?:"(?:(?:\\")|[^"])*"))?(?:\s+(.+))?$/;
+
+var parse = function(src) {
+	return Array.prototype.concat.apply([], src.split('<%').map(function(slice) {
+		return slice.split('%>');
+	})).map(function(data, i) {
+		if (i % 2 === 0) return data && {type:STATIC, value:data};
+
+		var line = data.split(' ');
+		var pre = line.shift();
+		var live = !!(pre[1] === '[');
+		var auto = pre === '{{' ? BLOCK_DECLARE : BLOCK_OVERRIDE;
+		var ctx = pre.replace(/[\{\[]+/, '{') + (line.length > 1 ? line.pop().replace(/[\}\]]+/, '}') : '');
+
+		line = line.join(' ');
+
+		if (pre === '')  return {type:LOGIC, value:line};
+		if (pre === '#') return null;
+		if (pre === '=') return {type:ESCAPE_EXPRESSION, value:line};
+		if (pre === '-') return {type:EXPRESSION, value:line};
+
+		line = (line.match(MATCH_BLOCK) || []).slice(1);
+		line = !line.length || (line[2] && !line[2]) ? {} : {
+			name:line[0],
+			url:line[1] && line[1].substr(1, line[1].length-2).replace(/\\(.)/g, '$1'), 
+			locals:line[2] && line[2].trim()
+		};
+
+		if (ctx === '{}' && line.name) return {type:auto, live:live, name:line.name, locals:line.locals, url:line.url, body:[]};
+		if (ctx === '{}' && line.url)  return {type:BLOCK_ANONYMOUS, url:line.url};
+		if (ctx === '{' && line.name)  return {type:auto, live:live, name:line.name, locals:line.locals, capture:1, body:[]};
+		if (ctx === '}') return {capture:-1};
+
+		throw new SyntaxError('could not parse: <%'+data+'%>');
+	}).filter(function(item) {
+		return item;
+	}).reduce(function reduce(result, node) {
+		var last = result[result.length-1];
+
+		if (!last || !last.capture) return result.concat(node);
+
+		last.capture += (node.capture || 0);
+		last.body = last.capture ? last.body.concat(node) : last.body.reduce(reduce, []);
+		return result;
+	}, []);
+};
+
+
+//console.log(JSON.stringify(parse('hello world <% var data = 42; %><%= data %>...<%{ foo }%><%{ "meh.html" }%><%{ test %>meh<%{ fpp %><%} %>test<%} %>'), null, '  '));
+console.log(JSON.stringify(parse(require('fs').readFileSync('example.html', 'utf-8')), null, '  '));
