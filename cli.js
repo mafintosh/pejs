2dc45260ffe01206f51a378d4e2e8a35ad1dd3eb
mafintosh <mathiasbuus@gmail.com>
2013-02-07 21:36:04 +0100

    output file support

--- cli.js
+++ cli.js
@@ -3,24 +3,34 @@
 var fs = require('fs');
 var pejs = require('./index');
 
-var filename = process.argv[2];
+var tree = process.argv.indexOf('--tree') > -1 || process.argv.indexOf('-t') > -1;
+var format = process.argv[process.argv.indexOf('--out')+1 || process.argv.indexOf('-o')+1 || -1];
+var filenames = process.argv.slice(2).filter(function(filename, i, filenames) {
+	return filename[0] !== '-' && (filenames[i-1] || '')[0] !== '-';
+});
 
-if (!filename || filename[0] === '-') {
-	console.error('usage: pejs filename');
+if (!filenames.length) {
+	console.error('usage: pejs filename [options]');
+	console.error('  --out to specify output file (%f.out.js)');
+	console.error('  --tree to output the parsed template tree');
+	process.exit(1);
+}
+if (filenames.length > 1 && !format) {
+	console.error('multiple input files requires an output format ie --out %f.out.js');
 	process.exit(1);
 }
 
-if (!fs.existsSync(filename)) {
-	console.error(filename+' does not exist');
-	process.exit(2);
-}
+filenames.forEach(function(filename) {
+	if (!fs.existsSync(filename)) {
+		console.error(filename+' does not exist');
+		process.exit(2);
+	}
+});
 
-var tree = process.argv.indexOf('--tree') > -1 || process.argv.indexOf('-t') > -1;
-
-filename = fs.realpathSync(filename);
+filenames = filenames.map(fs.realpathSync);
 
 if (tree) {
-	pejs.tree(filename, function(err, tree) {
+	pejs.tree(filenames[0], function(err, tree) {
 		if (err) {
 			console.error(err.message);
 			process.exit(3);
@@ -30,7 +40,20 @@ if (tree) {
 	return;
 }
 
-pejs.parse(filename, function(err, src) {
+if (format) {
+	filenames.forEach(function(filename) {
+		pejs.parse(filenames[0], function(err, src) {
+			if (err) {
+				console.error(err.message);
+				process.exit(3);
+			}
+			fs.writeFile(format.replace('%f', filename.split('/').pop()), src);
+		});
+	});
+	return;
+}
+
+pejs.parse(filenames[0], function(err, src) {
 	if (err) {
 		console.error(err.message);
 		process.exit(3);
